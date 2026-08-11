fefd85d73e13d73aff0e59f7e1bec38d1ffc1b5e
sharkthakftw <sharkthakftw@gmail.com>
2026-08-12 01:29:03 +0530

    fix: 25% random, 75% from selected categories

--- src/api.rs
+++ src/api.rs
@@ -293,25 +293,61 @@ struct WikiFeedResponse {
     query: Option<WikiFeedQuery>,
 }
 
+use rand::Rng;
+
 async fn fetch_feed_batch(client: &reqwest::Client) -> Result<Vec<FeedItem>, String> {
     let url = "https://en.wikipedia.org/w/api.php";
-    let res = client
-        .get(url)
-        .query(&[
-            ("action", "query"),
-            ("generator", "random"),
-            ("grnnamespace", "0"),
-            ("grnlimit", "5"),
-            ("prop", "extracts|categories"),
-            ("exintro", "1"),
-            ("explaintext", "1"),
-            ("clshow", "!hidden"),
-            ("cllimit", "15"),
-            ("format", "json"),
-        ])
-        .send()
-        .await
-        .map_err(|e| format!("network error: {}", e))?;
+    let profile = crate::feed::profile::FeedProfile::load();
+
+    let active_subcats = profile.get_active_subcategories();
+    let mut selected_cat = None;
+    if !active_subcats.is_empty() {
+        let mut rng = rand::thread_rng();
+        if rng.gen_range(0..100) < 80 {
+            let idx = rng.gen_range(0..active_subcats.len());
+            selected_cat = Some(active_subcats[idx].clone());
+        }
+    }
+
+    let res = if let Some(cat_name) = selected_cat {
+        let category_title = format!("Category:{}", cat_name);
+        client
+            .get(url)
+            .query(&[
+                ("action", "query"),
+                ("generator", "categorymembers"),
+                ("gcmtitle", &category_title),
+                ("gcmtype", "page"),
+                ("gcmlimit", "10"),
+                ("prop", "extracts|categories"),
+                ("exintro", "1"),
+                ("explaintext", "1"),
+                ("clshow", "!hidden"),
+                ("cllimit", "15"),
+                ("format", "json"),
+            ])
+            .send()
+            .await
+    } else {
+        client
+            .get(url)
+            .query(&[
+                ("action", "query"),
+                ("generator", "random"),
+                ("grnnamespace", "0"),
+                ("grnlimit", "5"),
+                ("prop", "extracts|categories"),
+                ("exintro", "1"),
+                ("explaintext", "1"),
+                ("clshow", "!hidden"),
+                ("cllimit", "15"),
+                ("format", "json"),
+            ])
+            .send()
+            .await
+    };
+
+    let res = res.map_err(|e| format!("network error: {}", e))?;
 
     let feed_resp: WikiFeedResponse = res
         .json()
