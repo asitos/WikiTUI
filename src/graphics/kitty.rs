4635b50b8ea783c4204d6a6f03f5f705bed6b011
sharkthakftw <sharkthakftw@gmail.com>
2026-08-30 16:40:15 +0530

    refactor(graphics): remove unused png rendering function

--- src/graphics/kitty.rs
+++ src/graphics/kitty.rs
@@ -96,37 +96,6 @@ pub fn render_kitty_image_from_path<W: Write>(
     Ok(())
 }
 
-pub fn render_kitty_png_chunked<W: Write>(
-    writer: &mut W,
-    png_base64: &str,
-    cols: u16,
-    rows: u16,
-) -> io::Result<()> {
-    let chunk_size = 4096;
-    let bytes = png_base64.as_bytes();
-    let total = bytes.len();
-    let mut offset = 0;
-
-    while offset < total {
-        let end = (offset + chunk_size).min(total);
-        let chunk = &png_base64[offset..end];
-        let more = if end < total { 1 } else { 0 };
-
-        if offset == 0 {
-            write!(
-                writer,
-                "\x1b_Ga=T,f=100,c={},r={},m={};{}\x1b\\",
-                cols, rows, more, chunk
-            )?;
-        } else {
-            write!(writer, "\x1b_Gm={};{}\x1b\\", more, chunk)?;
-        }
-
-        offset = end;
-    }
-
-    writer.flush()
-}
 
 pub fn render_kitty_rgba_chunked<W: Write>(
     writer: &mut W,
